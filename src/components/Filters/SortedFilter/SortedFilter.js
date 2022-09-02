e4bd9c495775ff55c057353ba887b8b736617ad2
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-09-02 02:34:33 -0700

    dropdown button added in sorted

--- src/components/Filters/SortedFilter/SortedFilter.js
+++ src/components/Filters/SortedFilter/SortedFilter.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const SortedFilter = ({sortByPriceHTL,sortByPriceLTH}) => {
+const SortedFilter = ({ sortByPriceHTL, sortByPriceLTH }) => {
     return (
         <div className='mt-4'>
-            <p>Sort Your Price</p>
-            <button className='btn btn-primary me-3' onClick={sortByPriceHTL}>Price High to low</button>
-            <button className='btn btn-success' onClick={sortByPriceLTH}>Price low to High</button>
+            <div class="btn-group" role="group">
+                <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Select Your Price Range
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                    <li><a class="dropdown-item" onClick={sortByPriceHTL}>High To Low</a></li>
+                    <li><a class="dropdown-item" onClick={sortByPriceLTH}>Low To High</a></li>
+                </ul>
+            </div>
+
         </div>
     );
 };
